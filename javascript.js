c8288d592747e97ca55eeb6614c3304065e17054
kevanpr <gracellepontanilla@gmail.com>
2024-05-15 16:13:14 +0800

    feat: add getComputerChoice function

---
+++ javascript.js
@@ -0,0 +1,20 @@
+function getComputerChoice () {
+    const randomNum = Math.floor(Math.random() * 3);
+    let choice;
+
+    switch (randomNum) {
+        case 0: 
+            choice = "rock";
+            break;
+        case 1: 
+            choice = "paper";
+            break;  
+        case 2: 
+            choice = "scissors";
+            break;
+    }
+    
+    return choice;
+}
+
+const compChoice = getComputerChoice();
