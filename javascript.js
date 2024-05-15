e9d0b42527cefde873f8c06d4583af0a52e8c35b
kevanpr <gracellepontanilla@gmail.com>
2024-05-15 22:00:18 +0800

    feat: add codes to playRound function

--- javascript.js
+++ javascript.js
@@ -25,9 +25,48 @@ function getHumanChoice () {
 let humanScore = 0;
 let computerScore = 0; 
 
-function playRound (humanChoice, compChoice) {
+/* rock | rock = 0 
+paper | paper = 0
+scissor | scissor = 0 
 
+paper | rock = 1 | 0 
+rock | paper = 0 | 1 
+
+paper | scissors = 0 | 1 
+scissors | paper = 1 | 0 
+
+rock | scissors = 1 | 0 
+scissors | rock = 0 | 1 
+ */
+
+function playRound(humanChoice, computerChoice) {
+    let humanChoice = humanSelection; 
+    let computerChoice = computerSelection; 
+
+    if (humanChoice == computerChoice) {
+        console.log("It's a tie. Try again");
+    } else if ((humanChoice == "paper") && (computerChoice == "rock")) {
+        humanScore++; 
+        console.log("You won! Paper beats rock!");
+    } else if ((humanChoice == "rock") && (computerChoice == "paper")) {
+        computerScore++;
+        console.log("You lose! Paper beats rock!");
+    } else if ((humanChoice == "paper") && (computerChoice == "scissors")) {
+        computerScore++; 
+        console.log("You lose! Scissors beat paper!");
+    } else if ((humanChoice == "scissors") && (computerChoice == "paper")) {
+        humanScore++;
+        console.log("You win! Scissors beat paper!");
+    } else if ((humanChoice == "rock") && (computerChoice == "scissors")) {
+        humanScore++;
+        console.log("You win! Rock beats scissors!");
+    } else if ((humanChoice == "scissors") && (computerChoice == "rock")) {
+        computerScore++;
+        console.log("You lose! Rock beats scissors!");
+    } else {
+        console.log("Wrong input. Please check your spelling and try again.");
+    }
 }
 
-const humanSelection = getHumanChoice();
+const humanSelection = getHumanChoice().toLocaleLowerCase();
 const computerSelection = getComputerChoice();
