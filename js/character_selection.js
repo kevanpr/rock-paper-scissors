61e8b6414c24a04efc70f61945a996c43fc9eb56
kevanpr <gracellepontanilla@gmail.com>
2024-07-12 22:20:52 +0800

    fix: set id on query selector

--- js/character_selection.js
+++ js/character_selection.js
@@ -1,23 +1,23 @@
-let characterSelection = undefined;
+let characterSelection = 0;
 
 const girlHero = document.querySelector('#girlHero');
-girlHero.addEventListener("click", () => characterSelection = 0);
+girlHero.addEventListener("click", () => characterSelection = 1);
 
 const wizardHero = document.querySelector('#wizardHero');
-wizardHero.addEventListener("click", () => characterSelection = 1);
+wizardHero.addEventListener("click", () => characterSelection = 2);
 
 const boyHero = document.querySelector('#boyHero');
-boyHero.addEventListener("click", () => characterSelection = 2);
+boyHero.addEventListener("click", () => characterSelection = 3);
 
 const startBtn = document.querySelector('#startBtn');
 btn.addEventListener("click", function () {
-    if (characterSelection = 0) {
+    if (characterSelection = 1) {
         window.location.assign("../webpages/girl_rps.html");
-    } else if (characterSelection = 1) {
-        window.location.assign("../webpages/wizard_rps.html");
     } else if (characterSelection = 2) {
+        window.location.assign("../webpages/wizard_rps.html");
+    } else if (characterSelection = 3) {
         window.location.assign("../webpages/boy_rps.html");
     } else {
-        alert ("Error");
+        alert ("Choose a character.");
     }
 });
