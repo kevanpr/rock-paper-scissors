da77b25cb7e4cc2eecd2bac0fc1c9fef8f56d5a0
kevanpr <gracellepontanilla@gmail.com>
2024-07-12 23:22:34 +0800

    fix: add window event listener to all listeners

--- js/character_selection.js
+++ js/character_selection.js
@@ -4,24 +4,53 @@ function changeValue (heroValue) {
     return characterSelection = heroValue;
 }
 
-let girlHero = document.querySelector('.girlHero');
-girlHero.addEventListener("click", () => characterSelection = 1);
-
-let wizardHero = document.querySelector('.wizardHero');
-wizardHero.addEventListener("click", () => characterSelection = 2);
-
-let boyHero = document.querySelector('.boyHero');
-boyHero.addEventListener("click", () => characterSelection = 3);
-
-const startBtn = document.querySelector('#startBtn');
-btn.addEventListener("click", function () {
-    if (characterSelection = 1) {
-        window.location.assign("../webpages/girl_rps.html");
-    } else if (characterSelection = 2) {
-        window.location.assign("../webpages/wizard_rps.html");
-    } else if (characterSelection = 3) {
-        window.location.assign("../webpages/boy_rps.html");
-    } else {
-        alert ("Choose a character.");
-    }
+window.addEventListener("DOMContentLoaded", (event) => {
+    let girlHero = document.querySelector('.girlHero');
+    girlHero.addEventListener("click", function () {
+        return characterSelection = 1;
+    });
 });
+
+window.addEventListener("DOMContentLoaded", (event) => {
+    let wizardHero = document.querySelector('.wizardHero');
+    wizardHero.addEventListener("click", function () {
+        return characterSelection = 2;
+    });
+});
+
+window.addEventListener("DOMContentLoaded", (event) => {
+    let boyHero = document.querySelector('.boyHero');
+    boyHero.addEventListener("click", function () {
+        return characterSelection = 3;
+    });
+});
+
+
+
+window.addEventListener("DOMContentLoaded", (event) => {
+    let startBtn = document.querySelector('.start');
+    startBtn.addEventListener("click", function () {
+        if (characterSelection = 1) {
+            window.location.assign("../webpages/girl_rps.html");
+        } else if (characterSelection = 2) {
+            window.location.assign("../webpages/wizard_rps.html");
+        } else if (characterSelection = 3) {
+            window.location.assign("../webpages/boy_rps.html");
+        }
+        });
+});
+
+
+/*
+if (startBtn) {
+    startBtn.addEventListener("click", function () {
+        if (characterSelection = 1) {
+            window.location.assign("../webpages/girl_rps.html");
+        } else if (characterSelection = 2) {
+            window.location.assign("../webpages/wizard_rps.html");
+        } else if (characterSelection = 3) {
+            window.location.assign("../webpages/boy_rps.html");
+        }
+        });
+} */
+
