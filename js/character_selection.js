3a9cabb329f4c5a284f95257f92fea51e3198ac7
kevanpr <gracellepontanilla@gmail.com>
2024-07-14 22:11:20 +0800

    fix: change DOMContentLoad to DOMContentLoaded

--- js/character_selection.js
+++ js/character_selection.js
@@ -1,33 +1,24 @@
 let characterSelection = 0;
 
-function changeValue (heroValue) {
-    return characterSelection = heroValue;
-}
-
 window.addEventListener("DOMContentLoaded", (event) => {
+
     let girlHero = document.querySelector('.girlHero');
     girlHero.addEventListener("click", function () {
         return characterSelection = 1;
     });
-});
 
-window.addEventListener("DOMContentLoaded", (event) => {
     let wizardHero = document.querySelector('.wizardHero');
     wizardHero.addEventListener("click", function () {
-        return characterSelection = 2;
+        return characterSelection = 2
     });
-});
 
-window.addEventListener("DOMContentLoaded", (event) => {
     let boyHero = document.querySelector('.boyHero');
     boyHero.addEventListener("click", function () {
         return characterSelection = 3;
     });
-});
 
+    /*
 
-
-window.addEventListener("DOMContentLoaded", (event) => {
     let startBtn = document.querySelector('.start');
     startBtn.addEventListener("click", function () {
         if (characterSelection = 1) {
@@ -38,19 +29,20 @@ window.addEventListener("DOMContentLoaded", (event) => {
             window.location.assign("../webpages/boy_rps.html");
         }
         });
+
+    let chosenHero = document.querySelector('.chosenHero');
+    let cH = "";
+
+    if (characterSelection = 1) {
+        cH = document.createTextNode("Alifir");
+    } else if (characterSelection = 2) {
+        cH = document.createTextNode("Klein");
+    } else if (characterSelection = 3) {
+        cH = document.createTextNode("Henriette");
+    } else if (characterSelection = 0) {
+        cH = document.createTextNode("None");
+    }
+
+    chosenHero.appendChild(cH); 
+    */
 });
-
-
-/*
-if (startBtn) {
-    startBtn.addEventListener("click", function () {
-        if (characterSelection = 1) {
-            window.location.assign("../webpages/girl_rps.html");
-        } else if (characterSelection = 2) {
-            window.location.assign("../webpages/wizard_rps.html");
-        } else if (characterSelection = 3) {
-            window.location.assign("../webpages/boy_rps.html");
-        }
-        });
-} */
-
