19f50c7310bf7970c9d68b56f68aacdb135b2077
kevanpr <gracellepontanilla@gmail.com>
2024-07-14 23:07:35 +0800

    fix: add textContent to chosenHero node to output chosen hero

--- js/character_selection.js
+++ js/character_selection.js
@@ -4,6 +4,66 @@ window.addEventListener("DOMContentLoaded", (event) => {
 
     let girlHero = document.querySelector('.girlHero');
     girlHero.addEventListener("click", function () {
+        let chosenHero = document.querySelector('.chosenHero');
+        chosenHero.textContent = "Alifir";
+
+        
+    });
+
+    let wizardHero = document.querySelector('.wizardHero');
+    wizardHero.addEventListener("click", function () {
+        let chosenHero = document.querySelector('.chosenHero');
+        chosenHero.textContent = "Klein";
+
+        
+    });
+
+    let boyHero = document.querySelector('.boyHero');
+    boyHero.addEventListener("click", function () {
+        let chosenHero = document.querySelector('.chosenHero');
+        chosenHero.textContent = "Henriette";
+
+    });
+
+
+    /*
+    let girlHero = document.querySelector('.girlHero');
+    girlHero.addEventListener("click", function () {
+        let chosenHero = document.querySelector('.chosenHero');
+        cH = document.createTextNode("Alifir");
+        chosenHero.appendChild(cH); 
+
+        return characterSelection = 1;
+    });
+
+    let wizardHero = document.querySelector('.wizardHero');
+    wizardHero.addEventListener("click", function () {
+        let chosenHero = document.querySelector('.chosenHero');
+        cH = document.createTextNode("Klein");
+        chosenHero.appendChild(cH); 
+
+        return characterSelection = 2
+    });
+
+    let boyHero = document.querySelector('.boyHero');
+    boyHero.addEventListener("click", function () {
+        let chosenHero = document.querySelector('.chosenHero');
+        cH = document.createTextNode("Henriette");
+        chosenHero.appendChild(cH); 
+
+        return characterSelection = 3;
+    });
+
+
+    
+
+
+
+
+    /*
+
+    let girlHero = document.querySelector('.girlHero');
+    girlHero.addEventListener("click", function () {
         return characterSelection = 1;
     });
 
@@ -28,21 +88,27 @@ window.addEventListener("DOMContentLoaded", (event) => {
         } else if (characterSelection = 3) {
             window.location.assign("../webpages/boy_rps.html");
         }
-        });
-
-    let chosenHero = document.querySelector('.chosenHero');
-    let cH = "";
-
-    if (characterSelection = 1) {
-        cH = document.createTextNode("Alifir");
-    } else if (characterSelection = 2) {
-        cH = document.createTextNode("Klein");
-    } else if (characterSelection = 3) {
-        cH = document.createTextNode("Henriette");
-    } else if (characterSelection = 0) {
-        cH = document.createTextNode("None");
-    }
-
-    chosenHero.appendChild(cH); 
-    */
+        }); */
 });
+
+
+    /*
+    function changeHero (hero) {
+        switch (hero) {
+            case ('Alifir'): 
+                document.querySelector('.chosenHero').textContent = "Alifir";
+                document.querySelector('.start').window.location.assign("../webpages/girl_rps.html");
+                break;
+            case ('Klein'):
+                document.querySelector('.chosenHero').textContent = "Klein";
+                document.querySelector('.start').window.location.assign("../webpages/wizard_rps.html");
+                break;
+            case('Henriette'):
+                document.querySelector('.chosenHero').textContent = "Henriette";
+                document.querySelector('.start').window.location.assign("../webpages/boy_rps.html");
+                break;
+            default:
+                document.querySelector('.chosenHero').textContent = "";
+                break;
+        }
+    } */
